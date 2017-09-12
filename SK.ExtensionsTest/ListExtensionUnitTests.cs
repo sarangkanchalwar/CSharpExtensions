e2ea8273d0b67aae32ee23af0fa18fcbe47747a1
Sarang K <sarang@bloomcs.com>
2017-09-12 15:55:05 +0530

    Added tests for ListExtensions in ListExtensionUnitTests

---
+++ SK.ExtensionsTest/ListExtensionUnitTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using SK.CSharpExtensions;
+
+namespace SK.ExtensionsTest
+{
+    [TestClass]
+    public class ListExtensionUnitTests
+    {
+        private List<int> numbers = new List<int>()
+        {
+           { 55 },
+           { 12 },
+           { 41 },
+           { 71 },
+           { 22 },
+           { 93 },
+           { 34 },
+           { 88 },
+           { 97 },
+           { 24 },
+           { 12 },
+           { 23 },
+           { 85 },
+           { 74 },
+           { 51 },
+           { 88 },
+           { 92 },
+        };
+
+        [TestMethod]
+        public void IsFirstTest1()
+        {
+            Assert.IsTrue(numbers.IsFirst(55));
+        }
+
+        [TestMethod]
+        public void IsFirstTest2()
+        {
+            Assert.IsFalse(numbers.IsFirst(12));
+        }
+
+        [TestMethod]
+        public void IsLastTest1()
+        {
+            Assert.IsTrue(numbers.IsLast(92));
+        }
+
+        [TestMethod]
+        public void IsLastTest2()
+        {
+            Assert.IsFalse(numbers.IsLast(88));
+        }
+    }
+}
